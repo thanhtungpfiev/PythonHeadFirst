6484227d8b7543cd38931ae36ebae7c5eb491d02
tung.dao <tung.dao@lge.com>
2021-11-25 17:20:42 +0700

    Coding Chapter 4 v1

--- vsearch.py
+++ vsearch.py
@@ -1,5 +1,9 @@
-def search4vowels(word: str) -> set:
+def search4vowels(phrase: str) -> set:
     """Return any vowels found in a supplied word."""
     vowels = set('aeiou')
-    return vowels.intersection(set(word))
+    return vowels.intersection(set(phrase))
 
+
+def search4letters(phrase: str, letters: str) -> set:
+    """Return a set of the 'letters' found in 'phrases'"""
+    return set(letters).intersection(set(phrase))
