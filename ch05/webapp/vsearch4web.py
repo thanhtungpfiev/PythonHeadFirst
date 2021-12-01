eb95a03d93a60172f895d407743b82cfb08a9a1d
tung.dao <tung.dao@lge.com>
2021-12-01 18:00:19 +0700

    Coding Chapter 5 v4

--- ch05/webapp/vsearch4web.py
+++ ch05/webapp/vsearch4web.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
-
-
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
-    return str(search4letters('life, the universe, and everything!', 'eiru,!'))
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results,)
 
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
