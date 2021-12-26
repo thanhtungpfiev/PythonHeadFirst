9c76f6d2a7fd3f787a245cd2f1564cb0263317df
thanhtungpfiev <thanhtungpfiev@gmail.com>
2021-12-26 21:57:28 +0700

    Finish Coding chapter 6 Python HeadFirst

--- ch06/webapp/vsearch4web.py
+++ ch06/webapp/vsearch4web.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 app = Flask(__name__)
 
+
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req, " ", res, file=log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -19,13 +20,28 @@ def do_search() -> 'html':
                            the_title=title,
                            the_phrase=phrase,
                            the_letters=letters,
-                           the_results=results,)
+                           the_results=results, )
 
 
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_tilte='View Log',
+                           the_row_titles=titles,
+                           the_data=contents)
 
 
 if __name__ == '__main__':
